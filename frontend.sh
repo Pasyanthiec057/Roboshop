6a484b868b96247bc824f44392bb0287f432010d
Charan <rpasyanthi@gmail.com>
2021-02-10 16:15:09 +0530

    c7

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] Setup Nginx...."
+Component=Frontend
+echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m$(Component)\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mSetup Nginx....\e[0m"
+echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m$(Component)\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mInstalling Nginx....\e[0m"
--- mongo.sh
+++ mongo.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+Component=Mongo
 
-echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] Setup Mongo...."
+echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m$(Component)\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mSetup Mongo....\e[0m"
+echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m$(Component)\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mInstalling Mongo....\e[0m"
