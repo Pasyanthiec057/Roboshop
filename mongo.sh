edf3ca38b212bce768315f5ed4571faa1112a808
Charan <rpasyanthi@gmail.com>
2021-02-10 16:31:41 +0530

    c10

--- mongo.sh
+++ mongo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 Component=Mongo
 
-echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mSetup Mongo....\e[0m"
-echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mInstalling Mongo....\e[0m"
+echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mSetup ${Component}....\e[0m"
+echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mInstalling ${Component}....\e[0m"
+echo -e  "[\e[32mSUCCESS\e[0m] [\e[1;35m${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[1;32m${Component} installed successfully\e[0m"
+echo -e   "[\e[31mFAIL\e[0m] [\e[1;31m${Component}\e[0m] [\e[1;31m$(date "+%F %T")\e[0m] \e[1;31m${Component} installation failed \e[0m"
