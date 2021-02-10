a96f2ed2366e44e1b4d710d069166b5ddb59cd32
Charan <rpasyanthi@gmail.com>
2021-02-10 15:40:13 +0530

    c6

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date "+%f %t")\e[0m] Setup Nginx...."
+echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] Setup Nginx...."
