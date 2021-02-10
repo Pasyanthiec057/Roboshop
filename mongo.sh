3155e19e7b52ef2f4c4d572b87b042cb7cff9bad
Charan <rpasyanthi@gmail.com>
2021-02-10 16:17:32 +0530

    c8

--- mongo.sh
+++ mongo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 Component=Mongo
 
-echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m$(Component)\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mSetup Mongo....\e[0m"
-echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m$(Component)\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mInstalling Mongo....\e[0m"
+echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m$Component\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mSetup Mongo....\e[0m"
+echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m$Component\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mInstalling Mongo....\e[0m"
