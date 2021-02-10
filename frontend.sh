0b5423e805107c0ebf8a0b8333c2c35ce249f7d7
Charan <rpasyanthi@gmail.com>
2021-02-10 16:21:31 +0530

    c9

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 Component=Frontend
-echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m$Component\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mSetup Nginx....\e[0m"
-echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m$(omponent\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mInstalling Nginx....\e[0m"
+echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mSetup Nginx....\e[0m"
+echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m${omponent}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30mInstalling Nginx....\e[0m"
