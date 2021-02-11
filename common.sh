b64381514dba55a8f6915c329cd54bbaa0ccd92e
Charan <rpasyanthi@gmail.com>
2021-02-11 12:43:20 +0530

    auto pushing

--- common.sh
+++ common.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 Info(){
-  echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30m $1....\e[0m"
+  echo -e "[\e[1;32mINFO\e[0m] [\e[1;35m${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30m$1....\e[0m"
 }
 Success(){
-echo -e  "[\e[32mSUCCESS\e[0m][\e[1;35m ${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[1;32m $1  success\e[0m"
+  echo -e  "[\e[1;32mSUCC\e[0m][\e[1;35m${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[1;32m$1  success\e[0m"
 }
 Fail(){
-echo -e  "[\e[31mFAIL\e[0m]   [\e[1;31m${Component}\e[0m] [\e[1;31m$(date "+%F %T")\e[0m] \e[1;31m $1  failed \e[0m"
+  echo -e  "[\e[1;31mFAIL\e[0m [\e[1;31m${Component}\e[0m] [\e[1;31m$(date "+%F %T")\e[0m] \e[1;31m$1  failed \e[0m"
 }
 #mkdir robolog
 #cd robolog
