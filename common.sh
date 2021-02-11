e0c3ce0f84c5b01122ed103805e9b419a3d59080
Charan <rpasyanthi@gmail.com>
2021-02-11 10:12:52 +0530

    auto pushing

--- common.sh
+++ common.sh
@@ -3,17 +3,13 @@
 Info(){
   echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30m $1....\e[0m"
 }
-
 Success(){
 echo -e  "[\e[32mSUCCESS\e[0m][\e[1;35m ${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[1;32m $1 installed successfully\e[0m"
 }
-
 Fail(){
 echo -e  "[\e[31mFAIL\e[0m]   [\e[1;31m${Component}\e[0m] [\e[1;31m$(date "+%F %T")\e[0m] \e[1;31m $1 installation failed \e[0m"
 }
-
 User_Id=$(uid -u)
-
 case $User_Id in
   0)
     true
