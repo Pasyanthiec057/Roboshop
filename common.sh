2501150e5cefd72cd0b6ec01e609687bff2ce0b9
Charan <rpasyanthi@gmail.com>
2021-02-11 12:27:06 +0530

    auto pushing

--- common.sh
+++ common.sh
@@ -4,10 +4,10 @@ Info(){
   echo -e  "[\e[1;32mINFO\e[0m] [\e[1;35m${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[47;30m $1....\e[0m"
 }
 Success(){
-echo -e  "[\e[32mSUCCESS\e[0m][\e[1;35m ${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[1;32m $1 installed successfully\e[0m"
+echo -e  "[\e[32mSUCCESS\e[0m][\e[1;35m ${Component}\e[0m] [\e[1;36m$(date "+%F %T")\e[0m] \e[1;32m $1  success\e[0m"
 }
 Fail(){
-echo -e  "[\e[31mFAIL\e[0m]   [\e[1;31m${Component}\e[0m] [\e[1;31m$(date "+%F %T")\e[0m] \e[1;31m $1 installation failed \e[0m"
+echo -e  "[\e[31mFAIL\e[0m]   [\e[1;31m${Component}\e[0m] [\e[1;31m$(date "+%F %T")\e[0m] \e[1;31m $1  failed \e[0m"
 }
 #mkdir robolog
 #cd robolog
@@ -36,6 +36,8 @@ Res(){
       Fail "$2"
       ;;
 esac
+}
 
-
+Download(){
+  curl -s -L -o  /temp/${Component}.zip $1 &>> $Out_Path
 }
